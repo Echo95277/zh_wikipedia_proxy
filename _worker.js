4616333e6536189600c20cb19e6433d57eae28fd
Echo95277 <huangmanzong@gmail.com>
2024-06-06 18:57:52 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,8 +1,16 @@
 export default {
   async fetch(request, env) {
     const url = new URL(request.url);
-    url.host = "wikipedia.org";
-    // openai is already set all CORS heasders 
+    
+    // Extract the subdomain from the host
+    const subdomain = url.host.split('.')[0];
+    
+    // Check if the subdomain is 'zh' or 'en' and modify the host accordingly
+    if (subdomain === 'zh' || subdomain === 'en') {
+      url.host = subdomain + '.wikipedia.org';
+    }
+    
+    // openai is already set all CORS headers
     return fetch(url, {
       headers: request.headers,
       method: request.method,
