461ed5b17a327da1da76e504e02b3f9eb9d395b4
Echo95277 <huangmanzong@gmail.com>
2024-06-06 18:47:57 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     const url = new URL(request.url);
-    url.host = "wikidata.org";
+    url.host = "zh.wikipedia.org";
     // openai is already set all CORS heasders 
     return fetch(url, {
       headers: request.headers,
